f2d2f03ab2672351d6f50f520a32c9e3523d4fce
pulvera <pulverachelsyr@gmail.com>
2024-06-07 13:10:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// db
+import db from './_db.js'
+
+// types
+import { typeDefs } from './schema.js'
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
+
+/* 
+games {
+    title
+}
+*/
+
+// server setup 
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+    listen:  { port: 4000 }
+})
+
+console.log('Server ready at port', 4000)
